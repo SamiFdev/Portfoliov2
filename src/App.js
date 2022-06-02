10b7202f14e248653580b4d1d8fa3a68d9ab41d3
Sami Fares <djsamif@gmail.com>
2022-06-02 13:07:29 -0400

    made changes requested in PR comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import './App.css'
-// import NavBar from './components/NavBar';
-// import About from './components/About';
 
 function App() {
-    return (
-        <>
-            {/* <NavBar /> */}
-            <header className="App-header">{/* <About /> */}</header>
-        </>
-    )
+    return <main></main>
 }
 
 export default App
