936d6a6455fdba5653d76b8d098b9f1268cb2b0c
Sami Fares <djsamif@gmail.com>
2022-06-02 13:50:18 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-    return <main></main>
+    return (
+        <main>
+            <Navbar />
+        </main>
+    )
 }
 
 export default App
