2304b2f53c72d5373222f8fc455b10b1c5f8b0d3
Sami Fares <djsamif@gmail.com>
2022-09-08 14:58:12 -0400

    updated dl link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -25,7 +25,7 @@ function Footer() {
                     <FaLinkedin color="white" size={48} />
                 </a>
                 <a
-                    href="https://drive.google.com/file/d/1OJK_O-70pxaw6NQPhrP555ttwesZ1-JU/view?usp=sharing"
+                    href="https://drive.google.com/file/d/1CfVtRR6wnLoapxDJA6cx6j1wj1ob2xCq/view?usp=sharing"
                     download
                     target="blank"
                     rel="noreferrer">
